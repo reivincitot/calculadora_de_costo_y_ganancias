d67f1b8d5ea1296f64c001961d1a1d2e211491f5
Sebastian <reivincitot@gmail.com>
2025-05-12 18:30:26 -0400

    Comienzo desde 0 para mayor orden

--- core/domain/entities.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-
-class Producto(BaseModel):
-    sku: str
-    nombre: str
-    costo_unitario: float
-    fecha_creacion: datetime = Field(default_factory=datetime.now)
